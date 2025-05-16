26fbec3405b6b565d57518797bf6e1269529cac9
khrystynarep <khr.repetylo@gmail.com>
2025-05-16 22:04:42 +0300

    modified:   package.json         modified:   src/store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-
+process.env.NODE_ENV
 import rootReducer from './reducers';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers =
-    (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) ||
+    (process.env.NODE_ENV === 'development' ? globalThis.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) ||
     compose;
 
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
